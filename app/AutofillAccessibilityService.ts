0ea21496b826166cda5a23e5e9e2522f782c1636
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-05-28 22:16:31 -0400

    autofill service

--- app/AutofillAccessibilityService.ts
+++ app/AutofillAccessibilityService.ts
@@ -1,7 +1,7 @@
 @JavaProxy("com.tns.autofill.AutofillAccessibilityService")
 export class AutofillAccessibilityService extends android.accessibilityservice.AccessibilityService {
     onAccessibilityEvent(e: android.view.accessibility.AccessibilityEvent) {
-        console.log(e.getPackageName() + ' : ' + e.getEventType());
+        console.log('onAccessibilityEvent : ' + e.getPackageName() + ' : ' + e.getEventType());
     }
 
     onInterrupt() {
