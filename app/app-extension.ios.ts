68c041ed5dc4a0b19f4768ec5da504762e223769
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-08-21 21:58:38 -0400

    ios app extension

---
+++ app/app-extension.ios.ts
@@ -0,0 +1,11 @@
+export class LoadingViewController extends UIViewController {
+    viewDidAppear(animated: boolean) {
+        console.log('my view did appear');
+        super.viewDidAppear(animated);
+    }
+
+    viewDidLoad() {
+        console.log('my view did load');
+        super.viewDidLoad();
+    }
+}
