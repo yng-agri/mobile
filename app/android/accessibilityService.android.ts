8d7bb49cf74b6e57b4686ba0c9df30210b94bfc7
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-17 17:15:38 -0400

    access service container from accessibility service

--- app/android/accessibilityService.android.ts
+++ app/android/accessibilityService.android.ts
@@ -1,7 +1,22 @@
+import { ServiceContainer } from '../../services/serviceContainer';
+import { StorageService } from 'jslib/abstractions/storage.service';
+import { StateService } from 'jslib/abstractions/state.service';
+import { I18nService } from 'jslib/abstractions/i18n.service';
+import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
+
 @JavaProxy('com.tns.AccessibilityService')
 export class AccessibilityService extends android.accessibilityservice.AccessibilityService {
-    onAccessibilityEvent(e: android.view.accessibility.AccessibilityEvent) {
+    async onAccessibilityEvent(e: android.view.accessibility.AccessibilityEvent) {
         console.log('onAccessibilityEvent : ' + e.getPackageName() + ' : ' + e.getEventType());
+        const serviceContainer: ServiceContainer = this.getApplicationContext()['serviceContainer'];
+        const storageService = serviceContainer.resolve<StorageService>('storageService');
+        const stateService = serviceContainer.resolve<StateService>('stateService');
+        const i18nService = serviceContainer.resolve<I18nService>('i18nService');
+        const platformUtilsService = serviceContainer.resolve<PlatformUtilsService>('platformUtilsService');
+        console.log('state: ' + (await stateService.get<string>('hello')));
+        console.log('i18n: ' + i18nService.t('hello'));
+        console.log('platform: ' + platformUtilsService.getDevice());
+        console.log('storage: ' + (await storageService.get<string>('hello')));
     }
 
     onInterrupt() {
