782bc5b9b37a2d5db944409bb5947746d3aec864
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-09-26 09:45:42 -0400

    stub out ios crypto

--- plugins/bw-crypto/src/bw-crypto.ios.ts
+++ plugins/bw-crypto/src/bw-crypto.ios.ts
@@ -1,3 +1,42 @@
 export class BwCrypto {
+    pbkdf2(password: string | ArrayBuffer, salt: string | ArrayBuffer, algorithm: 'sha256' | 'sha512',
+        iterations: number): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
 
+    hash(value: string | ArrayBuffer, algorithm: 'sha1' | 'sha256' | 'sha512' | 'md5'): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    hmac(value: ArrayBuffer, key: ArrayBuffer, algorithm: 'sha1' | 'sha256' | 'sha512'): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    aesEncrypt(data: ArrayBuffer, iv: ArrayBuffer, key: ArrayBuffer): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    aesDecrypt(data: ArrayBuffer, iv: ArrayBuffer, key: ArrayBuffer): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    rsaEncrypt(data: ArrayBuffer, publicKey: ArrayBuffer, algorithm: 'sha1' | 'sha256'): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    rsaDecrypt(data: ArrayBuffer, privateKey: ArrayBuffer, algorithm: 'sha1' | 'sha256'): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    rsaExtractPublicKey(privateKey: ArrayBuffer): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
+
+    rsaGenerateKeyPair(length: 1024 | 2048 | 4096): Promise<[ArrayBuffer, ArrayBuffer]> {
+        return Promise.resolve(null);
+    }
+
+    randomBytes(length: number): Promise<ArrayBuffer> {
+        return Promise.resolve(null);
+    }
 }
