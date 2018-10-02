bf7cb16db9b8ced9a916fc5e187d4c4fcd91e0b7
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-10-02 10:00:44 -0400

    secure storage for iOS

--- plugins/bw-secure-storage/src/bw-secure-storage.ios.ts
+++ plugins/bw-secure-storage/src/bw-secure-storage.ios.ts
@@ -1,17 +1,67 @@
+const KeyChainService = 'com.8bit.bitwarden';
+const KeyChainAccessGroup = 'LTZ2PFU5D6.com.8bit.bitwarden';
+
 export class BwSecureStorage {
     init(options: any) {
-        // Nothing to do
+        // Nothing to init on iOS
     }
 
     get<T>(key: string): Promise<T> {
-        return Promise.resolve(null);
+        try {
+            const query = this.getRecordQuery(key);
+            const intRef = new interop.Reference<any>();
+            this.checkError(SecItemCopyMatching(query, intRef));
+            const b64 = (intRef.value as NSData).base64EncodedStringWithOptions(0);
+            return Promise.resolve(b64 as any);
+        } catch (e) {
+            return Promise.reject(e);
+        }
     }
 
-    save(key: string, obj: any): Promise<any> {
-        return Promise.resolve();
+    async save(key: string, obj: any): Promise<any> {
+        if (typeof (obj) !== 'string') {
+            throw new Error('Only base 64 strings can be stored in android key store.');
+        }
+        try {
+            await this.remove(key);
+            const query = this.getRecordQuery(key, obj);
+            this.checkError(SecItemAdd(query, null));
+            return Promise.resolve();
+        } catch (e) {
+            return Promise.reject(e);
+        }
     }
 
     remove(key: string): Promise<any> {
-        return Promise.resolve();
+        try {
+            const query = this.getRecordQuery(key);
+            this.checkError(SecItemDelete(query));
+            return Promise.resolve();
+        } catch (e) {
+            return Promise.reject(e);
+        }
+    }
+
+    private checkError(resultCode: number) {
+        if (resultCode !== errSecSuccess) {
+            throw new Error('Failed to execute key chain action. Result code: ' + resultCode);
+        }
+    }
+
+    private getRecordQuery(key: string, obj: any = null) {
+        const query = NSMutableDictionary.new();
+        query.setValueForKey(kSecClassGenericPassword, kSecClass);
+        query.setValueForKey(KeyChainAccessGroup, kSecAttrAccessGroup);
+        query.setValueForKey(KeyChainService, kSecAttrService);
+        query.setValueForKey(key, kSecAttrAccount);
+        if (obj == null) {
+            query.setValueForKey(kCFBooleanTrue, kSecReturnData);
+            query.setValueForKey(kSecMatchLimitOne, kSecMatchLimit);
+        }
+        else {
+            const data = NSData.alloc().initWithBase64EncodedStringOptions(obj, 0);
+            query.setValueForKey(data, kSecValueData);
+        }
+        return query;
     }
 }
