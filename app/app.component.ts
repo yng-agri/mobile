c1b14734b41387f643365a6f5ac3cee0d26f825e
Kyle Spearrin <kyle.spearrin@gmail.com>
2018-10-15 13:02:16 -0400

    remove app component template url

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html',
+    template: '<page-router-outlet></page-router-outlet>',
 })
 
 export class AppComponent { }
