da2e8575d1f45301d94f91e77b06d0cf38102ad0
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-02 15:02:48 -0500

    callback must prefix _

--- app/android/mainActivity.android.ts
+++ app/android/mainActivity.android.ts
@@ -7,44 +7,45 @@ import {
 export class MainActivity extends android.support.v7.app.AppCompatActivity {
     isNativeScriptActivity: boolean;
 
-    private callbacks: AndroidActivityCallbacks;
+    // callbacks must have underscore prefix for whatever reason
+    private _callbacks: AndroidActivityCallbacks;
 
     onCreate(savedInstanceState: android.os.Bundle): void {
         // Set the isNativeScriptActivity in onCreate (as done in the original NativeScript activity code)
         // The JS constructor might not be called because the activity is created from Android.
         this.isNativeScriptActivity = true;
-        if (!this.callbacks) {
+        if (!this._callbacks) {
             setActivityCallbacks(this);
         }
-        this.callbacks.onCreate(this, savedInstanceState, super.onCreate);
+        this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
     }
 
     onSaveInstanceState(outState: android.os.Bundle): void {
-        this.callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
+        this._callbacks.onSaveInstanceState(this, outState, super.onSaveInstanceState);
     }
 
     onStart(): void {
-        this.callbacks.onStart(this, super.onStart);
+        this._callbacks.onStart(this, super.onStart);
     }
 
     onStop(): void {
-        this.callbacks.onStop(this, super.onStop);
+        this._callbacks.onStop(this, super.onStop);
     }
 
     onDestroy(): void {
-        this.callbacks.onDestroy(this, super.onDestroy);
+        this._callbacks.onDestroy(this, super.onDestroy);
     }
 
     onBackPressed(): void {
-        this.callbacks.onBackPressed(this, super.onBackPressed);
+        this._callbacks.onBackPressed(this, super.onBackPressed);
     }
 
     onRequestPermissionsResult(requestCode: number, permissions: string[], grantResults: number[]): void {
-        this.callbacks.onRequestPermissionsResult(this, requestCode, permissions,
+        this._callbacks.onRequestPermissionsResult(this, requestCode, permissions,
             grantResults, undefined /*TODO: Enable if needed*/);
     }
 
     onActivityResult(requestCode: number, resultCode: number, data: android.content.Intent): void {
-        this.callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
+        this._callbacks.onActivityResult(this, requestCode, resultCode, data, super.onActivityResult);
     }
 }
