7877ce8460e2ed6fb9ceaac7beac0765130641e5
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-14 07:51:00 -0500

    observable property on login model

--- app/pages/login/login-page.ts
+++ app/pages/login/login-page.ts
@@ -2,7 +2,9 @@ import { NavigatedData, Page } from 'tns-core-modules/ui/page';
 
 import { LoginViewModel } from './login-view-model';
 
+import { MobileUtils } from '~/misc/mobileUtils';
+
 export function onNavigatingTo(args: NavigatedData) {
     const page = args.object as Page;
-    page.bindingContext = new LoginViewModel(page);
+    page.bindingContext = new LoginViewModel(page, MobileUtils.resolveService('authService'));
 }
--- app/pages/login/login-view-model.ts
+++ app/pages/login/login-view-model.ts
@@ -1,49 +1,21 @@
 import { Observable } from 'tns-core-modules/data/observable';
 import { Page } from 'tns-core-modules/ui/page/page';
 
+import { ObservableProperty } from '../../misc/observable-property.decorator';
+
 import { AuthService } from 'jslib/abstractions/auth.service';
 
 import { DeviceActionUtils } from '~/misc/deviceActionUtils';
 import { MobileUtils } from '~/misc/mobileUtils';
 
 export class LoginViewModel extends Observable {
-    protected authService: AuthService;
+    @ObservableProperty(['showPasswordIcon'])
+    showPassword = false;
+    @ObservableProperty() email = '';
+    @ObservableProperty() masterPassword = '';
 
-    private _email = '';
-    private _masterPassword = '';
-    private _showPassword = false;
-
-    constructor(private page: Page) {
+    constructor(private page: Page, private authService: AuthService) {
         super();
-        this.authService = MobileUtils.resolveService('authService');
-    }
-
-    get email(): string {
-        return this._email;
-    }
-
-    set email(value: string) {
-        this._email = value;
-        this.notifyPropertyChange('email', this.email);
-    }
-
-    get masterPassword(): string {
-        return this._masterPassword;
-    }
-
-    set masterPassword(value: string) {
-        this._masterPassword = value;
-        this.notifyPropertyChange('masterPassword', this.masterPassword);
-    }
-
-    get showPassword(): boolean {
-        return this._showPassword;
-    }
-
-    set showPassword(value: boolean) {
-        this._showPassword = value;
-        this.notifyPropertyChange('showPassword', this.showPassword);
-        this.notifyPropertyChange('showPasswordIcon', this.showPasswordIcon);
     }
 
     get showPasswordIcon() {
