7b26c4308b4bdd03d60b5cfa2287e44ea8190fc5
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-18 00:42:08 -0500

    accessibility service for autofill

--- app/android/accessibility/accessibilityHelpers.ts
+++ app/android/accessibility/accessibilityHelpers.ts
@@ -4,6 +4,7 @@ import { Credentials } from './credentials';
 export class AccessibilityHelpers {
     static lastCredentials: Credentials;
     static systemUiPackage = 'com.android.systemui';
+    static bitwardenTag = 'bw_access';
 
     static supportedBrowsers = new Map<string, Browser>([
         new Browser('com.android.chrome', 'url_bar'),
@@ -72,4 +73,170 @@ export class AccessibilityHelpers {
         'me.craftsapp.nlauncher',
         'com.ss.squarehome2',
     ]);
+
+    static getUri(root: android.view.accessibility.AccessibilityNodeInfo): string {
+        const rootPackageName = root.getPackageName();
+        let uri = 'androidapp://' + rootPackageName;
+        if (AccessibilityHelpers.supportedBrowsers.has(rootPackageName)) {
+            const browser = AccessibilityHelpers.supportedBrowsers.get(rootPackageName);
+            const nodeList = root.findAccessibilityNodeInfosByViewId(rootPackageName + ':id/' + browser.uriViewId);
+            if (nodeList != null && nodeList.size() > 0) {
+                let addressNode: android.view.accessibility.AccessibilityNodeInfo = nodeList.get(0);
+                if (addressNode != null) {
+                    uri = AccessibilityHelpers.extractUri(uri, addressNode, browser);
+                    addressNode = null;
+                }
+            }
+        }
+        return uri;
+    }
+
+    static extractUri(uri: string, addressNode: android.view.accessibility.AccessibilityNodeInfo,
+        browser: Browser): string {
+        if (addressNode == null) {
+            return uri;
+        }
+        const text = addressNode.getText().toString();
+        if (text == null) {
+            return uri;
+        }
+        uri = browser.getUriFunction(text);
+        if (uri != null && uri.indexOf('.') > -1) {
+            uri = uri.trim();
+            if (uri.indexOf('://') === -1 && uri.indexOf(' ') === -1) {
+                uri = ('http://' + uri);
+            } else if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
+                const parts = uri.split('. ');
+                if (parts.length > 1) {
+                    const urlPart = parts.find((p) => p.indexOf('http') === 0);
+                    if (urlPart != null) {
+                        uri = urlPart.trim();
+                    }
+                }
+            }
+        }
+        return uri;
+    }
+
+    // Check to make sure it is ok to autofill still on the current screen
+    static needsToAutofill(credentials: Credentials, currentUriString: string): boolean {
+        if (credentials == null) {
+            return false;
+        }
+        try {
+            const lastUri = new java.net.URL(credentials.lastUri);
+            const currentUri = new java.net.URL(currentUriString);
+            return lastUri.getHost() === currentUri.getHost();
+        } catch { }
+        return false;
+    }
+
+    static isEditText(n: android.view.accessibility.AccessibilityNodeInfo): boolean {
+        if (n == null) {
+            return false;
+        }
+        const className = n.getClassName();
+        if (className == null) {
+            return false;
+        }
+        return className.indexOf('EditText') > -1;
+    }
+
+    static fillCredentials(usernameNode: android.view.accessibility.AccessibilityNodeInfo,
+        passwordNodes: android.view.accessibility.AccessibilityNodeInfo[]): void {
+        if (AccessibilityHelpers.lastCredentials == null) {
+            return;
+        }
+        AccessibilityHelpers.fillEditText(usernameNode, AccessibilityHelpers.lastCredentials.username);
+        passwordNodes.forEach((n) =>
+            AccessibilityHelpers.fillEditText(n, AccessibilityHelpers.lastCredentials.password));
+    }
+
+    static fillEditText(editTextNode: android.view.accessibility.AccessibilityNodeInfo, value: string): void {
+        if (editTextNode == null || value == null) {
+            return;
+        }
+        const bundle = new android.os.Bundle();
+        bundle.putString(android.view.accessibility.AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, value);
+        editTextNode.performAction(android.view.accessibility.AccessibilityNodeInfo.ACTION_SET_TEXT, bundle);
+    }
+
+    static getWindowNodes(
+        n: android.view.accessibility.AccessibilityNodeInfo,
+        e: android.view.accessibility.AccessibilityEvent,
+        condition: (node: android.view.accessibility.AccessibilityNodeInfo) => boolean,
+        disposeIfUnused: boolean,
+        nodes: android.view.accessibility.AccessibilityNodeInfo[] = null,
+        recursionDepth = 0): android.view.accessibility.AccessibilityNodeInfo[] {
+        if (nodes == null) {
+            nodes = [];
+        }
+
+        let dispose = disposeIfUnused;
+        if (n != null && recursionDepth < 50) {
+            const resource = n.getViewIdResourceName();
+            const fromSystem = () => resource != null && resource.indexOf(AccessibilityHelpers.systemUiPackage) === 0;
+            if (n.getWindowId() === e.getWindowId() && !fromSystem() && condition(n)) {
+                dispose = false;
+                nodes.push(n);
+            }
+
+            for (let i = 0; i < n.getChildCount(); i++) {
+                const childNode = n.getChild(i);
+                if (i > 100) {
+                    android.util.Log.i(AccessibilityHelpers.bitwardenTag, 'Too many child iterations.');
+                    break;
+                } else if (childNode.hashCode() === n.hashCode()) {
+                    android.util.Log.i(AccessibilityHelpers.bitwardenTag,
+                        'Child node is the same as parent for some reason.');
+                } else {
+                    AccessibilityHelpers.getWindowNodes(childNode, e, condition, true, nodes, recursionDepth++);
+                }
+            }
+        }
+
+        if (dispose) {
+            n = null;
+        }
+        return nodes;
+    }
+
+    static getNodesAndFill(root: android.view.accessibility.AccessibilityNodeInfo,
+        e: android.view.accessibility.AccessibilityEvent,
+        passwordNodes: android.view.accessibility.AccessibilityNodeInfo[]) {
+        const allEditTexts = AccessibilityHelpers.getWindowNodes(root, e,
+            (n) => AccessibilityHelpers.isEditText(n), false);
+        let usernameEditText = AccessibilityHelpers.getUsernameEditText(allEditTexts);
+        AccessibilityHelpers.fillCredentials(usernameEditText, passwordNodes);
+        AccessibilityHelpers.disposeNodes(allEditTexts);
+        usernameEditText = null;
+    }
+
+    static getUsernameEditText(allEditTexts: android.view.accessibility.AccessibilityNodeInfo[]):
+        android.view.accessibility.AccessibilityNodeInfo {
+        let usernameEditText: android.view.accessibility.AccessibilityNodeInfo = null;
+        for (let i = 0; i < allEditTexts.length; i++) {
+            if (allEditTexts[i].isPassword()) {
+                break;
+            } else {
+                usernameEditText = allEditTexts[i];
+            }
+        }
+        return usernameEditText;
+    }
+
+    static printTestData(root: android.view.accessibility.AccessibilityNodeInfo,
+        e: android.view.accessibility.AccessibilityEvent): void {
+        const testNodes = AccessibilityHelpers.getWindowNodes(root, e,
+            (n) => n.getViewIdResourceName() != null && n.getText() != null, false);
+        const testNodesData = testNodes.map((n) => {
+            return { id: n.getViewIdResourceName().toString(), text: n.getText().toString() };
+        });
+        console.log(testNodesData);
+    }
+
+    static disposeNodes(nodes: android.view.accessibility.AccessibilityNodeInfo[]) {
+        nodes.forEach((n) => n = null);
+        nodes = null;
+    }
 }
--- app/android/accessibilityService.android.ts
+++ app/android/accessibilityService.android.ts
@@ -1,41 +1,35 @@
 import { ServiceContainer } from '../../services/serviceContainer';
 
 import { I18nService } from 'jslib/abstractions/i18n.service';
-import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
-import { StateService } from 'jslib/abstractions/state.service';
 import { StorageService } from 'jslib/abstractions/storage.service';
-import { Utils } from 'jslib/misc/utils';
-import { AccessibilityHelpers } from './accessibility/accessibilityHelpers';
-import { Browser } from './accessibility/browser';
 
-const BitwardenTag = 'bw_access';
+import { Utils } from 'jslib/misc/utils';
+
+import { AccessibilityHelpers } from './accessibility/accessibilityHelpers';
+
+declare let com: any;
+
 const AutoFillNotificationId = 34573;
-const SystemUiPackage = 'com.android.systemui';
 const BitwardenPackage = 'com.x8bit.bitwarden';
 const BitwardenWebsite = 'vault.bitwarden.com';
 
 @JavaProxy('com.tns.AccessibilityService')
 export class AccessibilityService extends android.accessibilityservice.AccessibilityService {
     private notificationChannel: android.app.NotificationChannel;
+    private serviceContainer: ServiceContainer;
     private storageService: StorageService;
+    private i18nService: I18nService;
     private lastNotificationTime = 0;
-    private lastNotificationUrl: string;
+    private lastNotificationUri: string;
     private launcherPackageNames: Set<string>;
     private lastLauncherSetBuilt: number;
     private rebuildLauncherSpan = 3600000; // 1 hour (in ms)
+    private settingAutofillPasswordField = false;
+    private settingAutofillPersistNotification = false;
+    private lastSettingsReload: number;
+    private settingsReloadSpan = 60000; // 1 minute (in ms)
 
     async onAccessibilityEvent(e: android.view.accessibility.AccessibilityEvent) {
-        console.log('onAccessibilityEvent : ' + e.getPackageName() + ' : ' + e.getEventType());
-        const serviceContainer: ServiceContainer = (this.getApplicationContext() as any).serviceContainer;
-        this.storageService = serviceContainer.resolve<StorageService>('storageService');
-        const stateService = serviceContainer.resolve<StateService>('stateService');
-        const i18nService = serviceContainer.resolve<I18nService>('i18nService');
-        const platformUtilsService = serviceContainer.resolve<PlatformUtilsService>('platformUtilsService');
-        console.log('state: ' + (await stateService.get<string>('hello')));
-        console.log('i18n: ' + i18nService.t('hello'));
-        console.log('platform: ' + platformUtilsService.getDevice());
-        console.log('storage: ' + (await this.storageService.get<string>('hello')));
-
         try {
             const powerManager: android.os.PowerManager = this.getSystemService(android.content.Context.POWER_SERVICE);
             if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT_WATCH &&
@@ -58,7 +52,9 @@ export class AccessibilityService extends android.accessibilityservice.Accessibi
                 return;
             }
 
-            // TODO: test nodes
+            // AccessibilityHelpers.printTestData(root, e);
+            this.loadServices();
+            this.loadSettings();
 
             let notificationManager: android.app.NotificationManager = this.getSystemService(
                 android.content.Context.NOTIFICATION_SERVICE);
@@ -66,24 +62,73 @@ export class AccessibilityService extends android.accessibilityservice.Accessibi
 
             switch (e.getEventType()) {
                 case android.view.accessibility.AccessibilityEvent.TYPE_VIEW_FOCUSED:
-                    const source = e.getSource();
-                    if (source == null || !source.isPassword()) { // TODO: OR NOT autofill password field setting
+                    const source1 = e.getSource();
+                    if (source1 == null || !source1.isPassword() || !this.settingAutofillPasswordField) {
                         break;
                     }
                     if (eventPackageName === BitwardenPackage) {
-                        // TODO: cancel notification
+                        this.cancelNotification(notificationManager);
                         break;
                     }
-                    // TODO: scan and cancel
+                    if (this.scanAndAutofill(root, e, notificationManager, cancelNotification)) {
+                        this.cancelNotification(notificationManager);
+                    }
                     break;
                 case android.view.accessibility.AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
                 case android.view.accessibility.AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                    const source2 = e.getSource();
+                    if (this.settingAutofillPasswordField && source2.isPassword()) {
+                        break;
+                    } else if (this.settingAutofillPasswordField && AccessibilityHelpers.lastCredentials == null) {
+                        if (Utils.isNullOrWhitespace(this.lastNotificationUri)) {
+                            this.cancelNotification(notificationManager);
+                            break;
+                        }
+                        const uri = AccessibilityHelpers.getUri(root);
+                        if (uri !== this.lastNotificationUri) {
+                            this.cancelNotification(notificationManager);
+                        } else if (uri.indexOf('androidapp://') === 0) {
+                            this.cancelNotification(notificationManager, 30000);
+                        }
+                        break;
+                    }
+
+                    if (eventPackageName === BitwardenPackage) {
+                        this.cancelNotification(notificationManager);
+                        break;
+                    }
+
+                    if (this.settingAutofillPersistNotification) {
+                        const uri = AccessibilityHelpers.getUri(root);
+                        if (uri != null && uri.indexOf(BitwardenWebsite) === -1) {
+                            let needToFill = AccessibilityHelpers.needsToAutofill(
+                                AccessibilityHelpers.lastCredentials, uri);
+                            if (needToFill) {
+                                const passwordNodes = AccessibilityHelpers.getWindowNodes(
+                                    root, e, (n) => n.isPassword(), false);
+                                needToFill = passwordNodes.length > 0;
+                                if (needToFill) {
+                                    AccessibilityHelpers.getNodesAndFill(root, e, passwordNodes);
+                                }
+                                AccessibilityHelpers.disposeNodes(passwordNodes);
+                            }
+                            if (!needToFill) {
+                                this.notifyToAutofill(uri, notificationManager);
+                                cancelNotification = false;
+                            }
+                        }
+                        AccessibilityHelpers.lastCredentials = null;
+                    } else {
+                        cancelNotification = this.scanAndAutofill(root, e, notificationManager, cancelNotification);
+                    }
+
+                    if (cancelNotification) {
+                        this.cancelNotification(notificationManager);
+                    }
                     break;
                 default:
                     break;
             }
-
-            // clean up
             notificationManager = null;
             root = null;
             e = null;
@@ -96,61 +141,93 @@ export class AccessibilityService extends android.accessibilityservice.Accessibi
         // Do nothing
     }
 
+    private scanAndAutofill(root: android.view.accessibility.AccessibilityNodeInfo,
+        e: android.view.accessibility.AccessibilityEvent, notificationManager: android.app.NotificationManager,
+        cancelNotification: boolean): boolean {
+        const passwordNodes = AccessibilityHelpers.getWindowNodes(root, e, (n) => n.isPassword(), false);
+        if (passwordNodes.length > 0) {
+            const uri = AccessibilityHelpers.getUri(root);
+            if (uri != null && uri.indexOf(BitwardenWebsite) === -1) {
+                if (AccessibilityHelpers.needsToAutofill(AccessibilityHelpers.lastCredentials, uri)) {
+                    AccessibilityHelpers.getNodesAndFill(root, e, passwordNodes);
+                } else {
+                    this.notifyToAutofill(uri, notificationManager);
+                    cancelNotification = false;
+                }
+            }
+            AccessibilityHelpers.lastCredentials = null;
+        } else if (AccessibilityHelpers.lastCredentials != null) {
+            setTimeout(() => {
+                AccessibilityHelpers.lastCredentials = null;
+            }, 1000);
+        }
+
+        AccessibilityHelpers.disposeNodes(passwordNodes);
+        return cancelNotification;
+    }
+
     private cancelNotification(notificationManager: android.app.NotificationManager, limit = 250): void {
         if (new Date().getTime() - this.lastNotificationTime < limit) {
             return;
         }
-        this.lastNotificationUrl = null;
+        this.lastNotificationUri = null;
         if (notificationManager != null) {
             notificationManager.cancel(AutoFillNotificationId);
         }
     }
 
-    private getUri(root: android.view.accessibility.AccessibilityNodeInfo): string {
-        const rootPackageName = root.getPackageName();
-        let uri = 'androidapp://' + rootPackageName;
-        if (AccessibilityHelpers.supportedBrowsers.has(rootPackageName)) {
-            const browser = AccessibilityHelpers.supportedBrowsers.get(rootPackageName);
-            const nodeList = root.findAccessibilityNodeInfosByViewId(rootPackageName + ':id/' + browser.uriViewId);
-            if (nodeList != null && nodeList.size() > 0) {
-                let addressNode: android.view.accessibility.AccessibilityNodeInfo = nodeList.get(0);
-                if (addressNode != null) {
-                    uri = this.extractUri(uri, addressNode, browser);
-                    addressNode = null;
-                }
-            }
+    private notifyToAutofill(uri: string, notificationManager: android.app.NotificationManager): void {
+        if (notificationManager == null || Utils.isNullOrWhitespace(uri)) {
+            return;
         }
-        return uri;
-    }
 
-    private extractUri(uri: string, addressNode: android.view.accessibility.AccessibilityNodeInfo,
-        browser: Browser): string {
-        if (addressNode == null) {
-            return uri;
-        }
-        const text = addressNode.getText();
-        if (text == null) {
-            return uri;
-        }
-        uri = browser.getUriFunction(text);
-        if (uri != null && uri.indexOf('.') > -1) {
-            uri = uri.trim();
-            if (uri.indexOf('://') === -1 && uri.indexOf(' ') === -1) {
-                uri = ('http://' + uri);
-            } else if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.KITKAT_WATCH) {
-                const parts = uri.split('. ');
-                if (parts.length > 1) {
-                    const urlPart = parts.find((p) => p.indexOf('http') === 0);
-                    if (urlPart != null) {
-                        uri = urlPart.trim();
-                    }
-                }
-            }
-        }
-        return uri;
-    }
+        const context = this.getApplicationContext();
+        const now = new Date().getTime();
+        const intent = new android.content.Intent(context, com.tns.AccessibilityActivity.class);
+        intent.putExtra('uri', uri);
+        intent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK |
+            android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP | android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        const pendingIntent = android.app.PendingIntent.getActivity(context, 0, intent,
+            android.app.PendingIntent.FLAG_UPDATE_CURRENT);
 
-    // TODO
+        const newAndroid = android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.KITKAT_WATCH;
+        const oAndNewerAndroid = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O;
+        const notificationContent = this.i18nService.t(
+            newAndroid ? 'autofillNotificationContent' : 'autofillNotificationContentOld');
+        const notificationIcon = context.getResources().getIdentifier('notification_sm',
+            'drawable', context.getPackageName());
+
+        let builder = new android.app.Notification.Builder(context);
+        builder.setSmallIcon(notificationIcon)
+            .setContentTitle(this.i18nService.t('bitwardenAutofillServiceTitle'))
+            .setContentText(notificationContent)
+            .setTicker(notificationContent)
+            .setWhen(now)
+            .setContentIntent(pendingIntent);
+
+        if (newAndroid) {
+            const primaryColor = context.getResources().getIdentifier('primary', 'color', context.getPackageName());
+            builder.setVisibility(android.app.Notification.VISIBILITY_SECRET)
+                .setColor(android.support.v4.content.ContextCompat.getColor(context, primaryColor));
+        }
+        if (oAndNewerAndroid) {
+            if (this.notificationChannel == null) {
+                this.notificationChannel = new android.app.NotificationChannel('bitwarden_autofill_service',
+                    this.i18nService.t('autofillServiceTitle'), android.app.Notification.PRIORITY_LOW);
+                notificationManager.createNotificationChannel(this.notificationChannel);
+            }
+            builder.setChannelId(this.notificationChannel.getId());
+        }
+
+        if (this.settingAutofillPersistNotification) {
+            builder.setPriority(-2);
+        }
+
+        this.lastNotificationTime = now;
+        this.lastNotificationUri = uri;
+        notificationManager.notify(AutoFillNotificationId, builder.build());
+        builder = null;
+    }
 
     private skipPackage(eventPackageName: string): boolean {
         if (Utils.isNullOrWhitespace(eventPackageName) ||
@@ -158,9 +235,10 @@ export class AccessibilityService extends android.accessibilityservice.Accessibi
             eventPackageName.indexOf('launcher') > -1) {
             return true;
         }
+        const now = new Date().getTime();
         if (this.launcherPackageNames == null || this.lastLauncherSetBuilt == null ||
-            (new Date().getTime() - this.lastLauncherSetBuilt) > this.rebuildLauncherSpan) {
-            this.lastLauncherSetBuilt = new Date().getTime();
+            (now - this.lastLauncherSetBuilt) > this.rebuildLauncherSpan) {
+            this.lastLauncherSetBuilt = now;
             const intent = new android.content.Intent(android.content.Intent.ACTION_MAIN);
             intent.addCategory(android.content.Intent.CATEGORY_HOME);
             const resolveInfo = this.getPackageManager().queryIntentActivities(intent, 0);
@@ -172,5 +250,28 @@ export class AccessibilityService extends android.accessibilityservice.Accessibi
             this.launcherPackageNames = new Set<string>(homePackages);
         }
         return this.launcherPackageNames.has(eventPackageName);
+    }
+
+    private loadServices(): void {
+        if (this.serviceContainer == null) {
+            this.serviceContainer = (this.getApplicationContext() as any).serviceContainer;
+        }
+        if (this.storageService == null) {
+            this.storageService = this.serviceContainer.resolve<StorageService>('storageService');
+        }
+        if (this.i18nService == null) {
+            this.i18nService = this.serviceContainer.resolve<I18nService>('i18nService');
+        }
+    }
+
+    private async loadSettings(): Promise<any> {
+        const now = new Date().getTime();
+        if (this.lastSettingsReload == null || (now - this.lastSettingsReload) > this.settingsReloadSpan) {
+            this.lastSettingsReload = now;
+            this.settingAutofillPasswordField = await this.storageService.get<boolean>(
+                'accessibilityAutofillPasswordField');
+            this.settingAutofillPersistNotification = await this.storageService.get<boolean>(
+                'accessibilityAutofillPersistNotification');
+        }
     }
 }
