b34e9c49e260647fb0f5af21e7956fb806788a5d
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-22 23:00:42 -0500

    move some events to appMain

--- app/app.ts
+++ app/app.ts
@@ -10,69 +10,6 @@ import { MobileUtils } from './misc/mobileUtils';
 
 const appMain = new AppMain();
 
-app.on(app.launchEvent, (args: app.LaunchEventData) => {
-    if (args.android) {
-        // For Android applications, args.android is an android.content.Intent class.
-        console.log('Launched Android application with the following intent: ' + args.android + '.');
-    } else if (args.ios !== undefined) {
-        // For iOS applications, args.ios is NSDictionary (launchOptions).
-        console.log('Launched iOS application with options: ' + args.ios);
-    }
-});
-
-app.on(app.suspendEvent, (args: app.ApplicationEventData) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log('Activity: ' + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log('UIApplication: ' + args.ios);
-    }
-});
-
-app.on(app.resumeEvent, (args: app.ApplicationEventData) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log('Activity: ' + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log('UIApplication: ' + args.ios);
-    }
-});
-
-app.on(app.displayedEvent, (args: app.ApplicationEventData) => {
-    console.log('displayedEvent');
-});
-
-app.on(app.orientationChangedEvent, (args: app.OrientationChangedEventData) => {
-    // 'portrait', 'landscape', 'unknown'
-    console.log(args.newValue);
-});
-
-app.on(app.exitEvent, (args: app.ApplicationEventData) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log('Activity: ' + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log('UIApplication: ' + args.ios);
-    }
-});
-
-app.on(app.lowMemoryEvent, (args: app.ApplicationEventData) => {
-    if (args.android) {
-        // For Android applications, args.android is an android activity class.
-        console.log('Activity: ' + args.android);
-    } else if (args.ios) {
-        // For iOS applications, args.ios is UIApplication.
-        console.log('UIApplication: ' + args.ios);
-    }
-});
-
-app.on(app.uncaughtErrorEvent, (args: app.UnhandledErrorEventData) => {
-    console.log('Error: ' + args.error);
-});
-
 if (app.ios != null) {
     class BitwardenApplicationDelegate extends UIResponder implements UIApplicationDelegate {
         static ObjCProtocols = [UIApplicationDelegate];
