d8d088388b17106e1bdf285a135936672f863604
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-01-22 23:32:12 -0500

    Settings with logout

--- app/pages/settings/settings-page.ts
+++ app/pages/settings/settings-page.ts
@@ -5,7 +5,17 @@ import {
 
 import { SettingsViewModel } from './settings-view-model';
 
+import { MobileUtils } from '~/misc/mobileUtils';
+
 export function onNavigatingTo(args: NavigatedData) {
     const page = args.object as Page;
-    page.bindingContext = new SettingsViewModel(page);
+    page.bindingContext = new SettingsViewModel(page, MobileUtils.resolveService('messagingService'),
+        MobileUtils.resolveService('i18nService'));
+}
+
+export function templateSelector(item: any, index: number, items: any[]) {
+    if (item.isHeader) {
+        return 'header';
+    }
+    return 'button';
 }
--- app/pages/settings/settings-view-model.ts
+++ app/pages/settings/settings-view-model.ts
@@ -2,13 +2,38 @@ import {
     EventData,
     Observable,
 } from 'tns-core-modules/data/observable';
+import { ItemEventData } from 'tns-core-modules/ui/list-view/list-view';
 import {
     Page,
     View,
 } from 'tns-core-modules/ui/page/page';
 
+import { I18nService } from 'jslib/abstractions/i18n.service';
+import { MessagingService } from 'jslib/abstractions/messaging.service';
+
 export class SettingsViewModel extends Observable {
-    constructor(private page: Page) {
+    items: any[];
+
+    constructor(private page: Page, private messagingService: MessagingService,
+        private i18nService: I18nService) {
         super();
+        this.items = [
+            { isHeader: true, name: 'Manage' },
+            { isHeader: true, name: 'Security' },
+            { isHeader: true, name: 'Account' },
+            { type: 'button', id: 'logout', name: this.i18nService.t('logOut') },
+            { isHeader: true, name: 'Tools' },
+            { isHeader: true, name: 'Other' },
+        ];
+    }
+
+    itemTapped(args: ItemEventData) {
+        const item = this.items[args.index];
+        const context: any = {};
+        if (item.isHeader) {
+            return;
+        } else if (item.id === 'logout') {
+            this.messagingService.send('logout', { confirm: true });
+        }
     }
 }
