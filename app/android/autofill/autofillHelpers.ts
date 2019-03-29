0e2042f7ed41209f1aaaa7430492f9a0fae01c47
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-03-29 08:46:31 -0400

    more autofill browsers from mozilla

--- app/android/autofill/autofillHelpers.ts
+++ app/android/autofill/autofillHelpers.ts
@@ -16,7 +16,7 @@ export class AutofillHelpers {
     static trustedBrowsers = new Set<string>([
         'org.mozilla.focus', 'org.mozilla.klar', 'com.duckduckgo.mobile.android',
     ]);
-    // These browsers work using the compatibility shim for the autofill framework
+    // These browsers work using compatibility mode for the autofill framework
     static compatBrowsers = new Set<string>([
         'org.mozilla.firefox', 'org.mozilla.firefox_beta', 'com.microsoft.emmx', 'com.android.chrome',
         'com.chrome.beta', 'com.android.browser', 'com.brave.browser', 'com.opera.browser', 'com.opera.touch',
@@ -25,6 +25,7 @@ export class AutofillHelpers {
         'com.sec.android.app.sbrowser', 'com.sec.android.app.sbrowser.beta', 'org.codeaurora.swe.browser',
         'com.amazon.cloud9', 'mark.via.gp', 'org.bromite.bromite', 'org.chromium.chrome', 'com.kiwibrowser.browser',
         'com.ecosia.android', 'com.opera.mini.native.beta', 'org.mozilla.fennec_aurora', 'com.qwant.liberty',
+        'org.mozilla.fenix', 'org.mozilla.reference.browser', 'org.mozilla.rocket',
     ]);
     // The URLs are blacklisted from autofilling
     static blacklistedUris = new Set<string>([
